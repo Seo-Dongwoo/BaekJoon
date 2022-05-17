35afc4b59379903cb180010e38aa99921b2642ed
SeoDongwoo <83106932+Seo-Dongwoo@users.noreply.github.com>
2022-05-17 12:32:19 +0900

    Create 1406.py
    1406-에디터

---
+++ 1406.py
@@ -0,0 +1,18 @@
+import sys
+l_stack = list(input())
+r_stack = []
+T = int(input())
+
+for i in range(T):
+    cmd = sys.stdin.readline().split()
+
+    if cmd[0] == 'L' and l_stack:
+        r_stack.append(l_stack.pop())
+    elif cmd[0] == 'D' and r_stack:
+        l_stack.append(r_stack.pop())
+    elif cmd[0] == 'B' and l_stack:
+        l_stack.pop()
+    elif cmd[0] == 'P':
+        l_stack.append(cmd[1])
+
+print("".join(l_stack + list(reversed(r_stack))))
