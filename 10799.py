4b709d63b82b5b6fd8ffb789e7b1f6a850554df2
SeoDongwoo <83106932+Seo-Dongwoo@users.noreply.github.com>
2022-05-21 17:07:57 +0900

    Create 10799.py
    10799-쇠막대

---
+++ 10799.py
@@ -0,0 +1,14 @@
+razor = list(input())
+answer = 0
+stack=[]
+
+for i in range(len(razor)):
+    if razor[i] == '(':
+        stack.append('(')
+    elif razor[i-1] == '(':
+        stack.pop()
+        answer += len(stack)
+    else:
+        stack.pop()
+        answer += 1
+print(answer)
