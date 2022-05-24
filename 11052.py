6cb4a9d3d118ea6458e5dd815dce11fd32943170
SeoDongwoo <83106932+Seo-Dongwoo@users.noreply.github.com>
2022-05-24 12:23:53 +0900

    11052-카드구매

---
+++ 11052.py
@@ -0,0 +1,13 @@
+N = int(input())
+card = [0]
+card += list(map(int, input().split())) # 카드 갯수 마다의 카드팩 가격
+
+dp = [0] * (N+1) #최대값
+dp[1] = card[1]
+dp[2] = max(card[2], card[1]*2)
+
+for i in range(3, N+1):
+    dp[i] = card[i] #자기 자신으로 만드는 경우
+    for j in range(1, i//2 + 1):
+        dp[i] = max(dp[i], dp[j] + dp[i-j])
+print(dp[N])
